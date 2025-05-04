a948155233fa950783a66692a6c57c06857b25e2
Wilbur <lixiaoyanghh@outlook.com>
2025-05-04 23:42:48 +0800

    更新run.py文件，替换原有的app.run()方法为WSGIServer，优化应用的启动方式以支持更高效的请求处理。

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 from files_server import create_app
+from gevent.pywsgi import WSGIServer
 
 app = create_app('production')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000) 
+    #app.run(host='0.0.0.0', port=5000) 
+    server = WSGIServer(('0.0.0.0', 5000), app)
+    server.serve_forever()
